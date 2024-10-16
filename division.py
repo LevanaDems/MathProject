d190e318cbc37eb1761c214d2246539865c2b45d
Levana Dems <levanadems8@gmail.com>
2024-10-16 16:27:01 +0200

    Ajout de la docstring de la fonction de division

--- division.py
+++ division.py
@@ -2,3 +2,8 @@ def di vi d e ( a , b ) :
 i f b == 0 :
 return ” Im p o s si bl e de d i v i s e r par z e ro ”
 return a / b
+
+
+”””
+Di vi si o n f u n c ti o n : Renvoie l e r e s u l t a t de l a d i v i s i o n de a par b .
+”””
